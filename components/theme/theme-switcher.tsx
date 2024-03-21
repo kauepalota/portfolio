6b5a1261e4ef014c620f4d7cda89ea64b15d61dc
Kauê Palota <kaueepalota@gmail.com>
2024-03-20 21:33:44 -0300

    fix: theme switcher bug

--- components/theme/theme-switcher.tsx
+++ components/theme/theme-switcher.tsx
@@ -9,12 +9,17 @@ export function ThemeSwitcher() {
   const { setTheme, theme } = useTheme()
 
   function toggleTheme() {
+    if (!theme) {
+      return
+    }
+
     setTheme(theme === 'dark' ? 'light' : 'dark')
   }
 
   return (
     <Button className="rounded-3xl p-2" onClick={toggleTheme} variant="ghost">
-      {theme === 'dark' ? <Sun /> : <Moon />}
+      <Sun className="rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+      <Moon className="absolute rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
     </Button>
   )
 }
